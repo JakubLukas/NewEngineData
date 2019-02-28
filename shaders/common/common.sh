94f1445b6e05e2268bdb5efdd0e4048261571c1e
JakubLukas <lukasik.lukasik@gmail.com>
2019-02-28 17:05:49 +0100

    normal mapping done

--- shaders/common/common.sh
+++ shaders/common/common.sh
@@ -5,3 +5,24 @@
 
 #include <bgfx_shader.sh>
 #include "shaderlib.sh"
+
+vec4 UnpackColor(float color)
+{
+	const float invByte = 1.0 / 256.0;
+	unsigned int c = floatBitsToUint(color);
+	return vec4(((c) & 0xFF) * invByte,
+		((c >> 8) & 0xFF) * invByte,
+		((c >> 16) & 0xFF) * invByte,
+		((c >> 24) & 0xFF) * invByte);
+}
+
+
+vec4 ComputeBlinnPhong(vec4 normal, vec4 viewDir, vec4 lightDir, vec4 lightColorDiff, vec4 lightColorSpec, vec4 matColorDiff, vec4 matColorSpec, float shininess)
+{
+	vec4 halfwayDir = normalize(lightDir + viewDir);
+
+	float diffuse = max(dot(lightDir, normal), 0.0);
+	float specular = pow(max(dot(normal, halfwayDir), 0.0), shininess);
+	
+	return (0.1 + diffuse) * matColorDiff * lightColorDiff + specular * matColorSpec * lightColorSpec;
+}
---
+++ shaders/cube.shader
@@ -0,0 +1,21 @@
+{
+	"varyings": [
+		"position",
+		"color0",
+		"texcoord0",
+		"normal",
+		"tangent"
+	],
+	"uniforms": [],
+	"textures": [
+		"t_diffuse",
+		"t_normal"
+	],
+	"lights": {
+		"directional": 1,
+		"point": 0
+	},
+
+	"vertexShader": "shaders/raw/cube/cube.vsr",
+	"fragmentShader": "shaders/raw/cube/cube.fsr"
+}
